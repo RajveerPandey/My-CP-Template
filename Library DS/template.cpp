122960c0871038e43ccddf2ce92a20e4a8860ce1
Rajveer Pandey <66234228+RajveerPandey@users.noreply.github.com>
2021-10-31 18:16:06 +0530

    Initial commit

---
+++ Library DS/template.cpp
@@ -0,0 +1,36 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define pb push_back
+#define fo(i,n) for(i=0; i<n; i++)
+#define ll long long
+#define PI 3.141592653589793238
+typedef pair<int, int> pii;
+typedef vector<int> vi;
+
+void fastIO() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+}
+
+void io() {
+    #ifndef ONLINE_JUDGE
+        freopen("input.txt", "r", stdin);
+        freopen("output.txt", "w", stdout);
+    #endif
+}
+
+int main() {
+    io();
+    fastIO();
+    int t, i, j, k, p, q, r, x, y, u, v, n, m;
+    
+    cin>>t;
+    while(t--) {
+    cout<<"Hello CP"<<endl;
+
+    }
+
+
+    return 0;
+}
